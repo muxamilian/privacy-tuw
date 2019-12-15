d2f1840ed5da6f35d4c196c6c11f2a628b6cf48e
Alexander Hartl <alexander.hartl@tuwien.ac.at>
2019-12-15 15:47:03 +0100

    Fixed adv_util_less_than_half bug

--- ex3/learn.py
+++ ex3/learn.py
@@ -1230,10 +1230,10 @@ def adv_until_less_than_half():
 			"ratio", final_ratios[attack_index],
 			"flow_accuracy", np.mean(np.round(numpy_sigmoid(np.array([item[-1] for item in orig_results[attack_index]])))),
 			"packet_accuracy", np.mean(np.round(numpy_sigmoid(np.array([sublist for l in orig_results[attack_index] for sublist in l])))),
-			"flow_distance", distances_flows[attack_index] if ratio <= THRESHOLD else 0,
-			"packet_distance", distances_packets[attack_index] if ratio <= THRESHOLD else 0,
-			"max_flow_distance", max_distance_flows[attack_index] if ratio <= THRESHOLD else 0,
-			"max_packet_distance", max_distance_packets[attack_index] if ratio <= THRESHOLD else 0
+			"flow_distance", distances_flows[attack_index] if final_ratios[attack_index] <= THRESHOLD else 0,
+			"packet_distance", distances_packets[attack_index] if final_ratios[attack_index] <= THRESHOLD else 0,
+			"max_flow_distance", max_distance_flows[attack_index] if final_ratios[attack_index] <= THRESHOLD else 0,
+			"max_packet_distance", max_distance_packets[attack_index] if final_ratios[attack_index] <= THRESHOLD else 0
 		)
 
 def eval_nn(data):
